9ce41e8391986e75a9fd2e9ce8100c268ac04103
Francesca Dreith <francesca.dreith@gmail.com>
2019-10-22 19:21:40 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,10 @@
+veg1 = macaroni
+veg2 = broccoli
+
 def meal_choice (veg1, veg2, protein="meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-veg1 = macaroni
-veg2 = broccoli
+
 
